e38f0e43c8e5f50f172cb3315d70067c9c0c947b
shankar <shankar.learnings07@gmail.com>
2025-06-22 00:59:01 +0530

    uploaded code

---
+++ src/test/java/com/example/demo/springboot_mysql_crud/SpringbootMysqlCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.springboot_mysql_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMysqlCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
